a5a38742a9ee6e0d0147fbf8ddfd6b4921e10855
bratin03 <bratinmondal689@gmail.com>
2025-03-04 12:57:40 +0530

    json library script

---
+++ code/utils/libraries/json.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Update the package list
+sudo apt-get update
+
+# Install the nlohmann-json3-dev package, which provides json.hpp
+sudo apt-get install -y nlohmann-json3-dev
+
+echo "json.hpp has been installed successfully."
