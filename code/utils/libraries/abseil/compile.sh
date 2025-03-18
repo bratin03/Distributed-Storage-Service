e39cb3f71ab2ec1ca4871c74d8577e5c9c0efd07
bratin03 <bratinmondal689@gmail.com>
2025-03-18 22:26:05 +0530

    Remove irrlevant libraries

--- code/utils/libraries/abseil/compile.sh
+++
@@ -1,1 +0,0 @@
-g++ -std=c++17 hello_abseil.cpp -o hello_abseil.out -labsl_strings -labsl_base -latomic -O2
--- code/utils/libraries/folly/compile.sh
+++
@@ -1,1 +0,0 @@
-g++ -std=c++17 main.cpp -L/usr/local/lib -lfolly -Wl,--whole-archive -lfmt -Wl,--no-whole-archive -lglog -levent -pthread -o fbstring_example.out -O3
